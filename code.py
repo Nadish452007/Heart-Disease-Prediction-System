daf43de3a27dbd2b9594796660f6e9910b2662e4
Nadish Thirukumaran <nadish452007@gmail.com>
2025-08-04 18:18:19 +0530

    Add files via upload

---
+++ code.py
@@ -0,0 +1,77 @@
+import pickle
+import streamlit as st
+from PIL.ImageFont import load_default
+from watchdog.utils import load_module
+import os
+
+working_dir = os.path.dirname(os.path.abspath(__file__))
+
+heart_disease_model = pickle.load(open('C:/Users/Nadish/Downloads/heart_disease_model.sav', 'rb'))
+
+
+st.markdown("<h1 style='text-align:center;'>🫀Heart Disease Prediction using ML</h1>",unsafe_allow_html=True)
+
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    age = st.text_input('Age')
+
+with col2:
+    sex = st.text_input('Gender')
+
+with col3:
+    cp = st.text_input('Chest Pain types')
+
+with col1:
+    trestbps = st.text_input('Resting Blood Pressure')
+
+with col2:
+    chol = st.text_input('Serum Cholestoral in mg/dl')
+
+with col3:
+    fbs = st.text_input('Fasting Blood Sugar > 120 mg/dl')
+
+with col1:
+    restecg = st.text_input('Resting Electrocardiographic results')
+
+with col2:
+    thalach = st.text_input('Maximum Heart Rate achieved')
+
+with col3:
+    exang = st.text_input('Exercise Induced Angina')
+
+with col1:
+    oldpeak = st.text_input('ST depression induced by exercise')
+
+with col2:
+    slope = st.text_input('Slope of the peak exercise ST segment')
+
+with col3:
+    ca = st.text_input('Major vessels colored by flourosopy')
+
+with col1:
+    thal = st.text_input('thal: 0 = normal; 1 = fixed defect; 2 = reversable defect')
+
+
+heart_diagnosis = ''
+
+if st.button('Heart Disease Test Result'):
+
+    try:
+        # Convert input to float
+        user_input = [float(x) for x in [age, sex, cp, trestbps, chol, fbs, restecg,
+                                         thalach, exang, oldpeak, slope, ca, thal]]
+
+
+        heart_prediction = heart_disease_model.predict([user_input])
+
+        if heart_prediction[0] == 1:
+            heart_diagnosis = 'The person is having heart disease'
+        else:
+            heart_diagnosis = 'The person does not have any heart disease'
+
+        st.success(heart_diagnosis)
+
+    except ValueError:
+        st.error("Please enter valid numeric values in all fields.")
